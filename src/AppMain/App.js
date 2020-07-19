48d37b779f87f1e55b19e29afa1615fbb6edd19b
Igifoshifo <matthew.abernathy@hotmail.com>
2020-07-18 22:08:20 -0400

    init

---
+++ src/AppMain/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class="grid-container">
+      <div class="Header">
+        <center>Header</center>
+      </div>
+      <div class="Main">
+        <center>Main</center>
+      </div>
+      <div class="Footer">
+        <center>Footer</center>
+      </div>
+    </div>
+  );
+}
+
+export default App;
