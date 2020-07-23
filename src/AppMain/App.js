f030943f84e52227156b337d6469813ace011714
Igifoshifo <matthew.abernathy@hotmail.com>
2020-07-23 15:24:15 -0400

    scrapped grid layout in favor of sidebar and sectioning

--- src/AppMain/App.js
+++ src/AppMain/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarContent, SidebarFooter, SidebarHeader } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 import './App.css';
 
 function App() {
   return (
-    <div class="grid-container">
-      <div class="Header">
-        <center>Header</center>
-      </div>
-      <div class="Main">
-        <center>Main</center>
-      </div>
-      <div class="Footer">
-        <center>Footer</center>
-      </div>
+    <div style = {{height:"100vh"}}>
+      <ProSidebar>
+        <SidebarHeader>
+          <center>Character Creator</center>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <Menu iconShape="square">
+            <MenuItem>Home</MenuItem>
+            <SubMenu title="DropDown PH" >
+              <MenuItem>Component 1</MenuItem>
+              <MenuItem>Component 2</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <center>Footer</center>
+        </SidebarFooter>
+      </ProSidebar>
     </div>
   );
 }
