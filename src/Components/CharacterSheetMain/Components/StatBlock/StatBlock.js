b9b2f84c0cbcb9cf2ba0797f947d8d36aabab79e
Igifoshifo <matthew.abernathy@hotmail.com>
2020-08-21 16:27:08 -0400

    Added components to main character sheet component. stat block and corresponding css have been added

--- src/Components/CharacterSheetMain/Components/StatBlock/StatBlock.js
+++ src/Components/CharacterSheetMain/Components/StatBlock/StatBlock.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './StatBlock.css'
 
-const StatBlock = ( {statName} ) => {
+const StatBlock = ({ statName }) => {
     return (
         <div className="statBlockContainer">
             <div className="statBlockTitle">
-                {statName}
+                {statName.toUpperCase()}
             </div>
             <div className="statBlockBonusField">
-                <Form>
-                    <Form.Control/>
+                <Form >
+                    <Form.Control size='lg' type="text"/>
                 </Form>
             </div>
             <div className="statBlockStatValueField">
-
+                <Form className="formShaping">
+                    <Form.Control size='sm' type="text"/>
+                </Form>
             </div>
         </div>
     );
