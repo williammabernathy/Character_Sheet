edb04976b00ea25bec52fab98b18ecf801ec116d
Igifoshifo <matthew.abernathy@hotmail.com>
2020-08-21 19:57:00 -0400

    minor sidebar tweaks

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import { ProSidebar, Menu, MenuItem, SidebarContent } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
-import { BiArrowToLeft, BiTransferAlt, BiBookContent, BiMessageDetail } from 'react-icons/bi';
+import { BiArrowToLeft, BiArrowFromLeft, BiTransferAlt, BiBookContent, BiMessageDetail } from 'react-icons/bi';
 import 'react-pro-sidebar/dist/css/styles.css';
 import './SideBar.css'
 
 const SideBar = () => {
     const [rtl, setRtl] = useState(false);
     const [collapsed, setCollapsed] = useState(false);
+    const [arrowIcon, setArrow] = useState(<BiArrowToLeft/>);
 
     // set the state to change if sidebar is toggled or not
     const handleCollapseToggle = () => {
-        if (collapsed === false)            
-        {
+        if (collapsed === false) {
             setCollapsed(true);
+            setArrow(<BiArrowFromLeft/>)
         }
         else {
             setCollapsed(false);
+            setArrow(<BiArrowToLeft/>)
         }
     }
 
     // set the state to change sidebar position from right to left or opposite
     const handleRTLToggle = () => {
-        if (rtl === false)           
-        {
+        if (rtl === false) {
             setRtl(true);
         }
         else {
@@ -40,19 +41,19 @@ const SideBar = () => {
 
                 <SidebarContent>
                     <Menu iconShape="circle">
-                        <MenuItem icon={<BiArrowToLeft />} onClick={() => handleCollapseToggle()}>
+                        <MenuItem icon={arrowIcon} onClick={() => handleCollapseToggle()}>
                             Minimize Sidebar
-                            </MenuItem>
+                        </MenuItem>
                         <MenuItem icon={<BiTransferAlt />} onClick={() => handleRTLToggle()}>
                             RightToLeft
-                            </MenuItem>
+                        </MenuItem>
                         <MenuItem icon={<BiBookContent />}>
                             Character Sheet
-                                <Link to="/controlpanel/charactersheet" />
+                            <Link to="/controlpanel/charactersheet" />
                         </MenuItem>
                         <MenuItem icon={<BiMessageDetail />}>
                             Chat
-                                <Link to="/controlpanel/chat" />
+                            <Link to="/controlpanel/chat" />
                         </MenuItem>
                     </Menu>
                 </SidebarContent>
